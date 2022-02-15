ba30d0ddf5254aea78676753334deca45236a5c6
datatto <58312941+datatto@users.noreply.github.com>
2022-02-14 21:17:17 -0300

    Add files via upload

---
+++ mapa.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = "pk.eyJ1IjoibWFwYW5hdmFsIiwiYSI6ImNrem1xcGdqeTFkeWsydm54bHhlOTR6eDEifQ.8jaPbnMtmE_b6hBa0IZBnw"
+var map =new mapboxgl.map({
+  container:"map" , 
+  style: "mapbox://styles/mapanaval/ckzmqyf8b000614pe6g5x6hry"
+  center: [-58.404257, -34.620608]
+  zoom:11.44
+});
